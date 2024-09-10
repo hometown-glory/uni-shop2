c42494ac8eb913dc210fc4483df0b0bc4dcbacc5
admin <1783736194@qq.com>
2024-09-10 16:41:32 +0800

    完成了商品列表的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -6,7 +6,8 @@
 			<!-- 左侧的滚动视图区域 -->
 			<scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
 				<block v-for="(item, i) in cateList" :key="i">
-					<view :class="['left-scroll-view-item', i === active ? 'active' : '']" @click="activeChanged(i)">{{item.cat_name}}</view>
+					<view :class="['left-scroll-view-item', i === active ? 'active' : '']" @click="activeChanged(i)">
+						{{item.cat_name}}</view>
 				</block>
 			</scroll-view>
 			<!-- 右侧的滚动视图区域 -->
@@ -17,7 +18,8 @@
 					<!-- 动态渲染三级分类的列表数据 -->
 					<view class="cate-lv3-list">
 						<!-- 三级分类 Item 项 -->
-						<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
+						<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3"
+							@click="gotoGoodsList(item3)">
 							<!-- 图片 -->
 							<image :src="item3.cat_icon"></image>
 							<!-- 文本 -->
@@ -57,7 +59,9 @@
 		methods: {
 			async getCateList() {
 				// 发起请求
-				const { data: res } = await uni.$http.get('/api/public/v1/categories')
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/categories')
 				// 判断是否获取失败
 				if (res.meta.status !== 200) return uni.$showMsg()
 				// 转存数据
@@ -91,64 +95,66 @@
 </script>
 
 <style lang="scss">
-.scroll-view-container {
-  display: flex;
+	.scroll-view-container {
+		display: flex;
 
-  .left-scroll-view {
-    width: 120px;
+		.left-scroll-view {
+			width: 120px;
 
-    .left-scroll-view-item {
-      line-height: 60px;
-      background-color: #f7f7f7;
-      text-align: center;
-      font-size: 12px;
+			.left-scroll-view-item {
+				line-height: 60px;
+				background-color: #f7f7f7;
+				text-align: center;
+				font-size: 12px;
 
-      // 激活项的样式
-      &.active {
-        background-color: #ffffff;
-        position: relative;
+				// 激活项的样式
+				&.active {
+					background-color: #ffffff;
+					position: relative;
 
-        // 渲染激活项左侧的红色指示边线
-        &::before {
-          content: ' ';
-          display: block;
-          width: 3px;
-          height: 30px;
-          background-color: #c00000;
-          position: absolute;
-          left: 0;
-          top: 50%;
-          transform: translateY(-50%);
-        }
-      }
-    }
-  }
-}
-.cate-lv2-title {
-  font-size: 12px;
-  font-weight: bold;
-  text-align: center;
-  padding: 15px 0;
-}
-.cate-lv3-list {
-  display: flex;
-  flex-wrap: wrap;
+					// 渲染激活项左侧的红色指示边线
+					&::before {
+						content: ' ';
+						display: block;
+						width: 3px;
+						height: 30px;
+						background-color: #c00000;
+						position: absolute;
+						left: 0;
+						top: 50%;
+						transform: translateY(-50%);
+					}
+				}
+			}
+		}
+	}
 
-  .cate-lv3-item {
-    width: 33.33%;
-    margin-bottom: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+	.cate-lv2-title {
+		font-size: 12px;
+		font-weight: bold;
+		text-align: center;
+		padding: 15px 0;
+	}
 
-    image {
-      width: 60px;
-      height: 60px;
-    }
+	.cate-lv3-list {
+		display: flex;
+		flex-wrap: wrap;
 
-    text {
-      font-size: 12px;
-    }
-  }
-}
+		.cate-lv3-item {
+			width: 33.33%;
+			margin-bottom: 10px;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			image {
+				width: 60px;
+				height: 60px;
+			}
+
+			text {
+				font-size: 12px;
+			}
+		}
+	}
 </style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		goods_list
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+				<my-goods :goods="goods" ></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +13,83 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数对象
+				queryObj: {
+					// 查询关键词
+					query: '',
+					// 商品分类Id
+					cid: '',
+					// 页码值
+					pagenum: 1,
+					// 每页显示多少条数据
+					pagesize: 10
+				},
+				// 商品列表的数据
+				goodsList: [],
+				// 总数量，用来实现分页
+				total: 0,
+				// 是否正在请求数据
+				isloading: false
 			};
+		},
+		onLoad(options) {
+			// 将页面参数转存到 this.queryObj 对象中
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			// 调用获取商品列表数据的方法
+			this.getGoodsList()
+		},
+		// 添加下拉刷新的处理函数
+		onPullDownRefresh() {
+			// 1. 重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+
+			// 2. 重新发起请求
+			this.getGoodsList(() => uni.stopPullDownRefresh())
+		},
+		methods: {
+			// 修改 getGoodsList 方法，添加回调函数参数
+			async getGoodsList(cb) {
+				// 打开节流阀
+				this.isloading = true
+				// 发起请求
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 关闭节流阀
+				this.isloading = false
+
+				// 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+
+				// 如果传入了回调函数，则调用
+				cb && cb()
+			},
+			// 点击跳转到商品详情页面
+			gotoDetail(goods) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+			}
+		},
+		// 触底的事件
+		onReachBottom() {
+			// 判断是否正在请求数据
+			if (this.isloading) return
+
+			// 判断是否还有下一页数据
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+
+			// 让页码值自增 +1
+			this.queryObj.pagenum += 1
+
+			// 重新获取列表数据
+			this.getGoodsList()
 		}
 	}
 </script>
